4d4c9bd6b1ddf9dc316738c9bb6ff86fc50b8afd
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-04 16:38:45 +0530

    Working with navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRoute } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/navbar';
 
 const App = () => {
   return (
-    <BrowserRoute>
+    <BrowserRouter>
       <div className="app">
         <Navbar />
       </div>
-    </BrowserRoute>
+    </BrowserRouter>
   );
 };
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import logo from './assets/logo.png';
 import { Link } from 'react-router-dom';
+import style from './styling/navbar.module.css';
 
-const navbar = () => {};
+const navbar = () => {
+  return (
+    <nav>
+      <div className={style.logo}>
+        <img src={logo} alt="logo" />
+        <h2>Space Travelers' Hub</h2>
+      </div>
+      <div className={style.list}>
+        <Link to="/rockets">Rockets</Link>
+        <Link to="/missions">Missions</Link>
+        <div className={style.line}></div>
+        <Link to="/profile">My Profile</Link>
+      </div>
+    </nav>
+  );
+};
 
 export default navbar;
