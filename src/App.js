6a3e2f4b3c380eb649ba63f571044363ba32c61f
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-04-04 16:06:04 +0530

    initiated redux and added navbar setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import React from 'react';
+import './App.css';
+import { BrowserRoute } from 'react-router-dom';
+import Navbar from './components/navbar';
+
+const App = () => {
   return (
-    <>
-      <h1>Hello from the react</h1>
-    </>
+    <BrowserRoute>
+      <div className="app">
+        <Navbar />
+      </div>
+    </BrowserRoute>
   );
-}
+};
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import logo from './assets/logo.png';
+import { Link } from 'react-router-dom';
+
+const navbar = () => {};
+
+export default navbar;
