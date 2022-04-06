d68ff5a96d1b61a63ba5dd06cfca085e032f4b3a
souad988 <59707859+souad988@users.noreply.github.com>
2022-04-06 20:41:34 +0000

    Display joined missions in myProfile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -5,15 +5,21 @@ import './styling/MyProfile.css';
 function MyProfile() {
   const data = useSelector((state) => state.rockets);
   const list = data.filter((item) => item.reserved === true);
+  const missions = useSelector((state) => state.missions);
+  const myMissions = missions.filter((item) => item.joined === true);
   return (
     <div className="my_profile_container">
       <section className="my_profile_missions">
         <h2>My Missions</h2>
-        <ul>
-          <li>Telstar</li>
-          <li>Ses</li>
-          <li>AsiaSat</li>
-        </ul>
+        {myMissions.length !== 0 ? (
+          <ul>
+            {
+            myMissions.map((mission) => (
+              <li key={mission.mission_id}>{mission.mission_name}</li>
+            ))
+          }
+          </ul>
+        ) : 'You didn\'t join any mission yet!'}
       </section>
       <section className="my_profile_rockets">
         <h2>My Rockets</h2>
